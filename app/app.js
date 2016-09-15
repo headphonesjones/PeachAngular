14ee858ab255ae95addf105e437452aab3cd4f65
Jordan Johnson <josamjohn@gmail.com>
2016-09-15 17:07:50 -0600

    Added every sub-page template. Also all pages now live under partials. Their routing and controllers are all within the app.js file. I think this cleans up the file structure a bit.

--- app/app.js
+++ app/app.js
@@ -11,9 +11,48 @@ angular.module('myApp', [
   'md.data.table'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-
-  $routeProvider.otherwise({redirectTo: '/dashboard'});
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+  $routeProvider
+      .when('/dashboard', {
+        templateUrl: 'partials/dashboard.html',
+        controller: 'DashCtrl'
+      })
+      .when('/schedule', {
+        templateUrl: '/partials/schedule.html',
+        controller:  'SchedCtrl'
+      })
+      .when('/schedule-tutor', {
+        templateUrl: '/partials/schedule_detail.html',
+        controller:  'SchedCtrl'
+      })
+      .when('/availability', {
+        templateUrl: '/partials/availability.html',
+        controller:  'AvailCtrl'
+      })
+      .when('/record', {
+        templateUrl: '/partials/record.html',
+        controller:  'RecordCtrl'
+      })
+      .when('/history', {
+        templateUrl: '/partials/history.html',
+        controller:  'HistCtrl'
+      })
+      .when('/account', {
+        templateUrl: '/partials/account.html',
+        controller:  'AccountCtrl'
+      })
+      .when('/settings', {
+        templateUrl: '/partials/settings.html',
+        controller:  'SettingsCtrl'
+      })
+      .when('/stats', {
+        templateUrl: '/partials/stats.html',
+        controller:  'StatsCtrl'
+      })
+      .otherwise({ redirectTo: '/dashboard' });
 }]);
 
 
@@ -24,3 +63,39 @@ angular.module('menuDemoWidth', ['ngMaterial']).config(function($mdIconProvider)
       .iconSet("call", 'img/icons/sets/communication-icons.svg', 24)
       .iconSet("social", 'img/icons/sets/social-icons.svg', 24);
 });
+
+angular.module('myApp.schedule', ['ngRoute']).controller('SchedCtrl', [function() {
+
+}]);
+
+angular.module('myApp.scheduleDetail', ['ngRoute']).controller('SchedCtrl', [function($scope) {
+
+}]);
+
+angular.module('myApp.dashboard', ['ngRoute']).controller('DashCtrl', [function() {
+
+}]);
+
+angular.module('myApp.availability', ['ngRoute']).controller('AvailCtrl', [function() {
+
+}]);
+
+angular.module('myApp.record', ['ngRoute']).controller('RecordCtrl', [function($scope) {
+
+}]);
+
+angular.module('myApp.history', ['ngRoute']).controller('HistCtrl', [function() {
+
+}]);
+
+angular.module('myApp.account', ['ngRoute']).controller('AccountCtrl', [function() {
+
+}]);
+
+angular.module('myApp.stats', ['ngRoute']).controller('StatsCtrl', [function($scope) {
+
+}]);
+
+angular.module('myApp.dashboard', ['ngRoute']).controller('DashCtrl', [function() {
+
+}]);
